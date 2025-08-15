29dca2030522ee9dd4e460c33610911140fb6103
ogawa-vaio <3223mq118@gmail.com>
2025-08-16 00:19:31 +0900

    a

--- c142.c
+++ c142.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.c>
+int main(void)
+{
+    char wantEatStr[11] = "";
+    int menuNum = 0;
+    char* menuList = "";
 
+    scanf("%s", wantEatStr);
+    scanf("%d", menuNum);
+
+    menuList = (char*)malloc(sizeof(char) * 10 * menuNum);
+    free(menuList);
+    return 0;
+}
