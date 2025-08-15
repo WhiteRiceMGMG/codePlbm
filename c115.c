a1b1170852e1d84ecc962a07658df8f7e62af70d
ogawa-vaio <3223mq118@gmail.com>
2025-08-15 23:57:05 +0900

    a

--- c115.c
+++ c115.c
@@ -3,6 +3,10 @@ int main(void)
 {
     int n = 0;
     int m = 0;
+    int i = 0;
     int* betweenCar;
     scanf("%d &d", &n, &m);
+    betweenCar = (int*)malloc(sizeof(int) * (n-1));
+    for()
+
 }
