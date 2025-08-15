5c5ff37b56ec252ced57eb6a09ce71fa93a45409
ogawa-vaio <3223mq118@gmail.com>
2025-08-16 00:23:48 +0900

    a

--- c142.c
+++ c142.c
@@ -5,11 +5,22 @@ int main(void)
     char wantEatStr[11] = "";
     int menuNum = 0;
     char* menuList = "";
+    int i = 0;
 
     scanf("%s", wantEatStr);
     scanf("%d", &menuNum);
 
-    menuList = (char*)malloc(sizeof(char) * 10 * menuNum);
+    menuList = (char*)malloc(sizeof(char) * 11 * menuNum);
+    
+    for(i = 0; i < menuNum; i++)
+    {
+        scanf("%s", &menuList[i]);
+    }
+
+    for(i = 0; i < menuNum; i++)
+    {
+        if("wantEatList" == "menuList[i]"){}
+    }
     free(menuList);
     return 0;
 }
