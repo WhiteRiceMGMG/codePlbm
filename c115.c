62efcbe3eb1bd4e846b514390ef874bfe90a8226
ogawa-vaio <3223mq118@gmail.com>
2025-08-16 00:07:36 +0900

    a

--- c115.c
+++ c115.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-    int n = 0;
-    int m = 0;
-    int i = 0;
+    int n   = 0;
+    int m   = 0;
+    int i   = 0;
+    int sum = 0;
     int* betweenCar;
+
     scanf("%d &d", &n, &m);
+    
     betweenCar = (int*)malloc(sizeof(int) * (n-1));
+    
     for(i = 0; i < n; i++)
     {
         scanf("%d", &betweenCar[i]);
