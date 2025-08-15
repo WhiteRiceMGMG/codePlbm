e88b8eec7cc50450b321ebf43166c69282def292
ogawa-vaio <3223mq118@gmail.com>
2025-08-16 01:32:05 +0900

    a

--- b168.c
+++ b168.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int aNum = 0;
-    int bNum = 0;
-    int cNum = 0;
+    int aNum     = 0;
+    int aNumList = {0};
+    int bNum     = 0;
+    int bNumList = {0};
+    int cNum     = 0;
+    int cNumList = {0};
+    scanf ("%d %d %d", &aNum, &bNum, &cNum);
     
+    return 0;
+
 }
