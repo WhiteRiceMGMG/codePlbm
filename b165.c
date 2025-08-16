e21017bfb4f81a84856a6ff0a69d8284b7565cc9
ogawa-vaio <3223mq118@gmail.com>
2025-08-17 00:04:59 +0900

    a

---
+++ b165.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+int main(void)
+{
+    int h = 0;
+    int w = 0;
+    int r = 0;
+    int c = 0;
+    int t = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    char status[26][26];
+    scanf("%d %d %d %d %d", &h, &w, &r, &c, &t);
+    
+    for(i = 0; i < h; i++)
+    {
+        for(j = 0; j < w; j++)
+        {
+            scanf("%c", &status[i][j]);
+        }
+    }
+    status[r][c] = 'B';
+    for(i = 1; i <= t; i++)
+    {
+        for(j = 0; j < h; j++)
+        {
+            for(k = 0; k < w; k++)
+            {
+                if(status[j][k] == 'B')
+                {
+                    status[j][k] = 'A';
+                    if((j == 0) && (k == 0) && (status[j][k+1] == '#'))
+                    {
+                        status[j][k+1] = 'B';
+                    } else 
+                    if((j == 0) && (k == 0) && (status[j+1][k] == '#'))
+                    {
+                        status[j+1][k] = 'B';
+                    } else
+                    if((j == 0) && (k == w) && (status[j][k-1] == '#'))
+                    {
+                        status[j][k-1] = 'B';
+                    } else
+                    if((j == 0) && (k == w) && (status[j+1][k] == '#'))
+                    {
+                        status[j+1][k] = 'B';
+                    } else
+                    if((j == h) && (k == 0) && (status[j-1][k] == '#'))
+                    {
+                        status[j-1][k] = 'B';
+                    } else
+                    if((j == h) && (k == 0) && (status[j][k+1] == '#'))
+                    {
+                        status[j][k+1] = 'B';
+                    } else
+                    if((j == h) && (k == w) && (status[j-1][k] == '#'))
+                    {
+                        
+                        status[j-1][k] = 'B';
+                    } else
+                    if((j == h) && (k == w) && (status[j][k-1] == '#'))
+                    {
+                        
+                        status[j][k-1] = 'B';
+                    } else
+                }
+                 
+            }
+        }
+    }
+}
--- b168.c
+++ b168.c
@@ -1,14 +1,135 @@
 #include <stdio.h>
+
+int tenPower(int a);
+int checkCalcResult(int i, int* array);
+
 int main(void)
 {
-    int aNum     = 0;
-    int aNumList = {0};
-    int bNum     = 0;
-    int bNumList = {0};
-    int cNum     = 0;
-    int cNumList = {0};
+    int aNum         = 0;
+    int aNumList[4]  = {0};
+    int aRsltList[8] = {0};
+    int bNum         = 0;
+    int bNumList[4]  = {0};
+    int bRsltList[8] = {0};
+    int cNum         = 0;
+    int cNumList[4]  = {0};
+    int cRsltList[8] = {0};
+    int i            = 0;
+    int j            = 0;
+    int k            = 0;
+    int aTemp        = 0;
+    int bTemp        = 0;
+    int cTemp        = 0;
+    int sumList[8]   = {0};
+    int sumNum       = 0;
+ 
     scanf ("%d %d %d", &aNum, &bNum, &cNum);
+    
+    for(i = 3; i >= 0; i--)
+    {
+        aNumList[i] = aNum / tenPower(i);
+        aNum        = aNum % tenPower(i);
+        //printf("%d\n", aNumList[i]);
+    }
+
+    for(i = 3; i >= 0; i--)
+    {
+        bNumList[i] = bNum / tenPower(i);
+        bNum        = bNum % tenPower(i);
+        //printf("%d\n", aNumList[i]);
+    }
+
+    for(i = 3; i >= 0; i--)
+    {
+        cNumList[i] = cNum / tenPower(i);
+        cNum        = cNum % tenPower(i);
+        //printf("%d\n", aNumList[i]);
+    }
+
+    for(i = 0; i < 8; i++)
+    {
+        for(j = 0; j < 8; j++)
+        {
+            for(k = 0; k < 8; k++)
+            {
+                aTemp = checkCalcResult(i, aNumList);
+                bTemp = checkCalcResult(j, bNumList);
+                cTemp = checkCalcResult(k, cNumList);
+                printf("%d %d %d", aTemp, bTemp, cTemp);
+                if((aTemp == bTemp) && (bTemp == cTemp))
+                {
+                    sumList[sumNum] = aTemp;
+                    sumNum++;
+                }
+                aTemp = 0;
+                bTemp = 0;
+                cTemp = 0;
+            }
+        }
+    }
+    if(sumNum == 0)
+    {
+        printf("NO\n");
+    } 
+    else 
+    {
+        printf("YES\n");
+        for(i = 0; i < sumNum; i++)
+        {
+            printf("%d\n", sumList[i]);
+        }
+    }
+
     
     return 0;
 
+}
+
+int tenPower(int a)
+{
+    int i = 0;
+    int temp = 1;
+    for(i = 1; i <= a; i++)
+    {
+        temp *= 10;
+    }
+    return temp;
+}
+
+int checkCalcResult(int i, int* array)
+{
+    int ans;
+        if(i == 0)
+        {
+            ans =   array[0] + array[1] + array[2] + array[3]; 
+        }
+        if(i == 1)
+        {
+            ans =   array[0] - array[1] + array[2] + array[3]; 
+        }
+        if(i == 2)
+        {
+            ans =   array[0] + array[1] - array[2] + array[3]; 
+        }
+        if(i == 3)
+        {
+            ans =   array[0] + array[1] + array[2] - array[3]; 
+        }
+        if(i == 4)
+        {
+            ans =   array[0] - array[1] - array[2] + array[3]; 
+        }
+        if(i == 5)
+        {
+            ans =   array[0] - array[1] + array[2] - array[3]; 
+        }
+        if(i == 6)
+        {
+            ans =   array[0] + array[1] - array[2] - array[3]; 
+        }
+        if(i == 7)
+        {
+            ans =   array[0] - array[1] - array[2] - array[3]; 
+        }
+    return ans;
 }
