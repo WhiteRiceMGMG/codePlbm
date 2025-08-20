ca3fbd69acd0f74b03dbe871ba34378c7c5e38eb
ogawa-vaio <3223mq118@gmail.com>
2025-08-20 20:50:24 +0900

    a

--- c170.c
+++ c170.c
@@ -5,5 +5,17 @@ int main(void)
     int n = 0;
     int m = 0;
     int array[101];
+    int i = 0;
+    int sum = 0;
+    int point = 0;
     scanf("%d %d", &n, &m);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    for(i = 0; i < n; i++)
+    {
+        point = array[i] / 100;
+        sum += point 
+    }
 }
