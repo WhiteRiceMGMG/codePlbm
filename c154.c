fc701e6d9446cd49b0a7f0f671a1d768b61dd2df
ogawa-vaio <3223mq118@gmail.com>
2025-08-21 22:17:17 +0900

    a

--- c154.c
+++ c154.c
@@ -4,11 +4,14 @@ int main(void)
 {
     int n = 0;
     int l = 0;
-    int array[3] = {0};
+    int array[110] = {0};
     int i = 0;
     
     scanf("%d %d", &n, &l);
     for(i = 0; i < n; i++)
-    {}
+    {
+        scanf("%d", &array[i]);
+    }
+    
  
 }
