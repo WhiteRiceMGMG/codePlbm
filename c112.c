fd6b323f014ce238ddadd7f9c2c94902d5cece81
ogawa-vaio <3223mq118@gmail.com>
2025-08-23 12:45:42 +0900

    a

--- c112.c
+++ c112.c
@@ -4,11 +4,14 @@ int main(void)
     int n = 0;
     int i = 0;
     int j = 0;
-    array[3][100] = 0;
+    array[100][3] = 0;
     
     scanf("%d", %n);
     for(i = 0; i < 3; i++)
     {
-        for(j = 0;)
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &array[i]);
+        }
     }
-}
+} 
