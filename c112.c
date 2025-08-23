c07f70ae93a5c5992869b2d5e761831c3d065077
ogawa-vaio <3223mq118@gmail.com>
2025-08-23 12:44:26 +0900

    a

--- c112.c
+++ c112.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+int main(void)
+{
+    int n = 0;
+    int i = 0;
+    int j = 0;
+    array[3][100] = 0;
+    
+    scanf("%d", %n);
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0;)
+    }
+}
