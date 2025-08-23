b90270ae08dd6d035a5a1e7787d5f7f20f2f426e
ogawa-vaio <3223mq118@gmail.com>
2025-08-23 12:49:23 +0900

    a

--- c112.c
+++ c112.c
@@ -4,14 +4,16 @@ int main(void)
     int n = 0;
     int i = 0;
     int j = 0;
-    array[100][3] = 0;
+    array[100][3] = {0};
     
-    scanf("%d", %n);
-    for(i = 0; i < 3; i++)
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) 
     {
-        for(j = 0; j < n; j++)
+        for(j = 0; j < 3; j++) 
         {
-            scanf("%d", &array[i]);
+            scanf("%d", &array[i][j]);   // 2次元配列に読み込み
         }
     }
+    
+
 } 
