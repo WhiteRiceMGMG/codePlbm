ee28195a6f012fa3123bff2dc14dcf641d77c2b5
ogawa-vaio <3223mq118@gmail.com>
2025-08-23 13:09:08 +0900

    a

--- c112.c
+++ c112.c
@@ -4,7 +4,10 @@ int main(void)
     int n = 0;
     int i = 0;
     int j = 0;
-    array[100][3] = {0};
+    int min = 0;
+    int max = 0;
+    int tmp = 0;
+    int array[100][3] = {0};
     
     scanf("%d", &n);
     for(i = 0; i < n; i++) 
@@ -14,6 +17,34 @@ int main(void)
             scanf("%d", &array[i][j]);   // 2次元配列に読み込み
         }
     }
-    
+    for(i = 0; i < n; i++)
+    {
+        tmp = array[i][0] 
+        + array[i][1] 
+        + (24-array[i][2]); 
+
+        //+ ((array[i][2] - (array[i][0] + array[i][1]) + 24) % 24);
+
+        if(i == 0)
+        {
+            max = tmp;
+            min = tmp;
+        }
+
+        if(max < tmp)
+        {
+            max = tmp;
+        }
+
+        if(min > tmp)
+        {
+            min = tmp;
+        }
+
+    }
+
+    printf("%d\n", min);
+    printf("%d", max);
+
 
 } 
