4422c6fd3c57a0fba25a3d33ac7d0e0cb7fa9839
ogawa-vaio <3223mq118@gmail.com>
2025-08-24 01:18:15 +0900

    a

--- c016.c
+++ c016.c
@@ -3,7 +3,7 @@ int main(void)
 {
     int i = 0;
     int j = 0;
-    char c = '';
+    char c = 'a';
     char str[100] = "";
     scanf("%s", str);
     char conpile[7][3] = 
@@ -11,14 +11,14 @@ int main(void)
         {'A', '4'},{'E', '3'},{'G', '6'},{'I', '1'},
         {'O', '0'}, {'S', '5'}, {'Z', '2'}
     };
-    while(c == '\0')
+    while(c != '\0')
     {
         c = str[j];
         for(i=0; i<7; i++)
         {
             if(c == conpile[i][0])
             {
-                str[i] = conpile[i][0];
+                str[i] = conpile[i][1];
             }
         }
         j++;
